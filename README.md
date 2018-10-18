9186fa4b3ece8b65caac004068af4a611b938556
Leon <32615985+leonhess@users.noreply.github.com>
2018-10-18 14:30:32 +0200

    added all the stuff

---
+++ README.md
@@ -0,0 +1,7 @@
+# EveVerboseKillmail - A python package for easy killmail processing
+
+run:
+````
+python3 setup.py sdist
+pip3 install dist/package_name.tar.gz
+````
---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='eve-verbose-killmail',
+    version='0.1_dev2',
+    packages=['eve_verbose_killmail'],
+    package_dir={'eve_verbose_killmail': 'src/eve_verbose_killmail'},
+    package_data={'eve_verbose_killmail': ['ressources/*.csv']},
+    license=open('LICENSE').read(),
+    long_description=open('README.md').read(),
+    author='Leon Hess',
+    author_email='',
+    url='',
+    install_requires=[
+        'requests'
+    ],
+)
