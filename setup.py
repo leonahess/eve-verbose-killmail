3514d90c4413616300e52040b95eab4ad1e3fbc4
Leon <32615985+leonhess@users.noreply.github.com>
2019-08-03 03:10:24 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,17 +2,15 @@ from distutils.core import setup
 
 setup(
     name='eve-verbose-killmail',
-    version='0.1.2',
+    version='0.2',
     packages=['eve_verbose_killmail'],
     package_dir={'eve_verbose_killmail': 'src/eve_verbose_killmail'},
     package_data={'eve_verbose_killmail': ['ressources/*.csv']},
     license=open('LICENSE').read(),
-    long_description=open('README.md').read(),
     author='Leon Hess',
     author_email='leon.hess@mailbox.tu-dresden.de',
     url='https://github.com/leonhess/eve-verbose-killmail',
     install_requires=[
         'requests'
     ],
-    download_url='https://github.com/leonhess/eve-verbose-killmail/archive/v0.1.2.tar.gz'
 )
